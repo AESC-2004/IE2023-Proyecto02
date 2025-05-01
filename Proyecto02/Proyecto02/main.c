fd3435ff0fe313e0f1ba9c5a14708b177674acf5
AESC-2004 <esc22235@uvg.edu.gt>
2025-04-30 19:27:21 -0600

    Subida inicial de archivos.

---
+++ Proyecto02/Proyecto02/main.c
@@ -0,0 +1,18 @@
+/*
+ * Proyecto02.c
+ *
+ * Created: 30/04/2025 07:25:47 p. m.
+ * Author : ang50
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
