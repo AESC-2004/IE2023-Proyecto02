d48147070368c3368174b57beb64fa5b91f9d6bf
AESC-2004 <esc22235@uvg.edu.gt>
2025-05-13 20:38:53 -0600

    Actualización del archivo principal del proyecto.

--- Proyecto02/Proyecto02/main.c
+++ Proyecto02/Proyecto02/main.c
@@ -18,16 +18,52 @@
 /*********************************************************************************************************************************************/
 
 /*********************************************************************************************************************************************/
-// Variables and constants
+// Variables, constants and data structures
 
-uint8_t Motors_MUX	= 0;
-uint8_t TIM0_Millis	= 0;
+// ADC variables
+uint8_t ADC_Count		= 0;							// Used for selecting which channel the ADC should read
+uint8_t ADC_Lec			= 0;							// Used for keeping the last ADC Lecture
 
-uint8_t ADC_Lec		= 0;
+// Decoder variables
+#define MANUAL			  0								// A "define" that'll come handy for establishing the mode
+#define ADAFRUIT		  1								// A "define" that'll come handy for establishing the mode
+uint8_t Mode			= MANUAL;						// Used for establishing the motor's positions mode
+typedef struct {
+	uint8_t	Adafruit[7];
+	uint8_t Manual[4];
+	uint8_t Usable[7];
+} MotorDecoding;
+MotorDecoding Motors;									// A struct for organizing them motor's positions depending on the mode
+uint8_t TIM0_Count		= 0;							// Used for knowing which Selector's signal to send to the decoder
+	
+// UART data structure
+#define EncodedDataBufferSize 16
+char	EncodedData[EncodedDataBufferSize];				// Buffer used for keeping the incoming Adafruit data
 
+// PWM mapping list
+#define PWM_TABLE_SIZE 256
+uint8_t ADCH_to_PWM[PWM_TABLE_SIZE] = {					// List kept in RAM for accessing the correct OCR1A value to set depending on the ADC Lectures
+	8,8,8,8,8,8,8,8,8,9,9,9,9,9,9,9,
+	9,10,10,10,10,10,10,10,10,10,11,11,11,11,11,11,
+	11,11,12,12,12,12,12,12,12,12,12,13,13,13,13,13,
+	13,13,13,14,14,14,14,14,14,14,14,14,15,15,15,15,
+	15,15,15,15,16,16,16,16,16,16,16,16,16,17,17,17,
+	17,17,17,17,17,18,18,18,18,18,18,18,18,18,19,19,
+	19,19,19,19,19,19,20,20,20,20,20,20,20,20,20,21,
+	21,21,21,21,21,21,21,22,22,22,22,22,22,22,22,22,
+	23,23,23,23,23,23,23,23,24,24,24,24,24,24,24,24,
+	24,25,25,25,25,25,25,25,25,26,26,26,26,26,26,26,
+	26,26,27,27,27,27,27,27,27,27,28,28,28,28,28,28,
+	28,28,28,29,29,29,29,29,29,29,29,30,30,30,30,30,
+	30,30,30,30,31,31,31,31,31,31,31,31,32,32,32,32,
+	32,32,32,32,32,33,33,33,33,33,33,33,33,34,34,34,
+	34,34,34,34,34,34,35,35,35,35,35,35,35,35,36,36,
+	36,36,36,36,36,36,36,37,37,37,37,37,37,37,37,38
+};
 /*********************************************************************************************************************************************/
 // Function prototypes
-void SETUP();
+void	SETUP();
+void	UART_ParseAdafruitFeedData();
 
 /*********************************************************************************************************************************************/
 // Main Function
@@ -37,6 +73,32 @@ int main(void)
 	
 	while (1)
 	{
+		// Checking for incoming DATA from Adafruit
+		UART_ParseAdafruitFeedData();
+		
+		// Depending on the mode, values are updated to MotorDecoding.Usable[]
+		// However, no matter the mode, Motors[5,6,7,8] are always driven by Adafruit
+		for (uint8_t i = 4; i < 7; i++) { Motors.Usable[i] = Motors.Adafruit[i];}
+		
+		switch (Mode)
+		{
+			case MANUAL:
+			{
+				for (uint8_t i = 0; i < 4; i++) {
+					Motors.Usable[i] = Motors.Manual[i];
+				}
+				break;
+			}
+			case ADAFRUIT:
+			{
+				for (uint8_t i = 0; i < 4; i++) {
+					Motors.Usable[i] = Motors.Adafruit[i];
+				}
+				break;
+			}
+			default: break;
+		}
+		
 	}
 }
 
@@ -51,71 +113,211 @@ void SETUP()
 	CLKPR	|= (1 << CLKPCE);
 	CLKPR	= (0 << CLKPCE) | (0 << CLKPS3) | (1 << CLKPS2) | (0 << CLKPS1) | (0 << CLKPS0);
 	
-	// Enabling PB0 as output for them motors
-	DDRB	|= (1 << DDB0);
 	
-	// Enabling PC0,1 as outputs for the motors MUX
-	DDRC	|= (1 << DDC0) | (1 << DDC3);
+	// Decoder
+	//DDRB	|= (1 << DDB5) | (1 << DDB4) | (1 << DDB3) | (1 << DDB2);
+	DDRB	|= (1 << DDB4) | (1 << DDB3) | (1 << DDB2);
 	
-	// Establishing a PWM with TIM1 
-	tim1_init(TIM1_CHANNEL_A, TIM1_PRESCALE_8, TIM1_MODE_CTC_OCR1A, 0, TIM1_COM_OC1x_DISCONNECTED, 0, TIM1_OC1x_DISABLE);
-	//tim1_oc_interrupt_enable(TIM1_CHANNEL_A);
+	// ADC
+	adc_init(ADC_REF_AVCC, ADC_PRESCALE_8, ADC_LEFT_ADJUST, ADC_CHANNEL_ADC7, ADC_INTERRUPT_ENABLE, ADC_AUTO_TRIGGER_DISABLE, ADC_TRIGGER_FREE_RUNNING);
+	DIDR0  |= (1 << ADC5D) | (1 << ADC4D);
 	
-	// Initiating TIM0 for MUX standards
-	tim0_init(TIM_8b_CHANNEL_A, TIM0_PRESCALE_8, TIM_8b_MODE_CTC_OCRA, 250, TIM_8b_COM_OCnx_DISCONNECTED, 0, TIM_8b_OCnx_DISABLE);
+	// Initiating TIM0 in CTC mode for DECODING standards
+	tim0_init(TIM_8b_CHANNEL_A, TIM0_PRESCALE_64, TIM_8b_MODE_CTC_OCRA, 39, TIM_8b_COM_OCnx_DISCONNECTED, 0, TIM_8b_OCnx_DISABLE);
 	tim_8b_oc_interrupt_enable(TIM_8b_NUM_0, TIM_8b_CHANNEL_A);
 	
-	// Settin an ADC lecture
-	adc_init(ADC_REF_AVCC, ADC_PRESCALE_8, ADC_LEFT_ADJUST, ADC_CHANNEL_ADC1, ADC_INTERRUPT_ENABLE, ADC_AUTO_TRIGGER_ENABLE, ADC_TRIGGER_FREE_RUNNING); 
+	// Initiating TIM1 in CTC mode for controlling them motors with OCR1A
+	tim1_init(TIM1_CHANNEL_A, TIM1_PRESCALE_64, TIM1_MODE_CTC_OCR1A, 0xFFFF, TIM1_COM_OC1x_DISCONNECTED, 0, TIM1_OC1x_DISABLE);
+	tim1_oc_interrupt_enable(TIM1_CHANNEL_A);
+	
+	
+	// Initiating UART communication
+	// UART 8b, no parity, 1 stop bit, 9600 baud rate
+	uart_init(USART_SPEED_DOUBLE, USART_CHARACTER_SIZE_8b, USART_PARITY_MODE_DISABLED, USART_STOP_BIT_1b, USART_MULTIPROCESSOR_COMMUNICATION_MODE_DISABLED, 12);
+	usart_rx_interrupt_enable();
 	
 	// Activating interrupts
 	sei();
 	
-	// Starting the ADC
+	// Initiating the first ADC Lecture
 	adc_start_conversion();
+}
+
+// UART_ParseAdafruitFeedData function. The incoming Adafruit data shall be parsed for knowing to which index of the Motors.Adafruit struct should the incoming value
+// should be kept. Afterwards, the USART Receive Buffer is flushed for keeping more data.
+{
+	if ((usart_get_received_length() > 0) && (((usart_get_received_byte((uint8_t)(usart_get_received_length() - 1))) == '\r') || ((usart_get_received_byte((uint8_t)(usart_get_received_length() - 1))) == '\n')))
+	{
+		// Using the EncodedData Receive Buffer for receiving the incoming data
+		usart_receive_string(EncodedData);
+		
+		// Starting the parsing process
+		
+		// Every DATA incoming shall have the following structure: "#X:VAL;"
+		// If the first received character is not "#", or the third one is not ":", the Data is trashed
+		if ((EncodedData[0] != '#') || (EncodedData[2] != ':')) {usart_rx_buffer_flush(); return;}
+		
+		// If the DATA follows the structure, it is needed to know if the ID is correct
+		// Then, the ID is extracted from the EncodedData Receive Buffer
+		// As in ASCII all characters are correctly listed (A=65, B=66, C=67, ...), the following code shall look if the ID is valid
+		// If 7 motors are to be controlled, the only possible ID's are 'A', 'B', 'C', 'D', 'E', 'F' and 'G'
+		// If any other character for the ID is used (i.e. 'H' or 'c'), subtracting 'A' from the ID will not give a result from 0 to 7
+		// If so, the DATA is trashed
+		char id = EncodedData[1];
+		uint8_t index = id - 'A';								// If the ID is correct, index should keep values from 0 to 7
+		if (index >= 8) {usart_rx_buffer_flush(); return;}
+		
+		// If the DATA follows and structure AND the ID is correct, VAL is turned into a string
+		uint8_t DATA_Value = (uint8_t)atoi(&EncodedData[3]);	// 'atoi' turns VAL into a string up until ';'
+		
+		// Data is copied to the correct Adafruit[] index
+		switch (index)
+		{
+			case 0: {Motors.Adafruit[0] = DATA_Value; break;}
+			case 1: {Motors.Adafruit[1] = DATA_Value; break;}
+			case 2: {Motors.Adafruit[2] = DATA_Value; break;}
+			case 3: {Motors.Adafruit[3] = DATA_Value; break;}
+			case 4: {Motors.Adafruit[4] = DATA_Value; break;}
+			case 5: {Motors.Adafruit[5] = DATA_Value; break;}
+			case 6: {Motors.Adafruit[6] = DATA_Value; break;}
+			//case 7: {Motors.Adafruit[7] = DATA_Value; break;}
+			
+			default: break;
+		}
+		
+		usart_rx_buffer_flush();
+	}
 }
 
 /*********************************************************************************************************************************************/
 // Interrupt routines
 
-ISR(TIMER0_COMPA_vect)
+// UART receive interrupt routine. "receive_bytes" function is used.
+ISR(USART_RX_vect)
 {
-	TIM0_Millis++;
-	if (TIM0_Millis == 10) TIM0_Millis = 0;
-	if (Motors_MUX == 0 && TIM0_Millis == 0)
-	{
-		PORTC	&= ~(1 << PORTC0);
-		tim1_ocr_value(TIM1_CHANNEL_A, ADC_Lec);
-		tim1_tcnt_value(0);
-		tim1_oc_interrupt_enable(TIM1_CHANNEL_A);
-		PORTC	|= (1 << PORTC3);
-		PORTB	|= (1 << PORTB0);
-		Motors_MUX	= 1;
-		
-	} else if (Motors_MUX == 1 && TIM0_Millis == 1)
-	{
-		PORTC	&= ~(1 << PORTC3);
-		tim1_ocr_value(TIM1_CHANNEL_A, 50);
-		tim1_tcnt_value(0);
-		tim1_oc_interrupt_enable(TIM1_CHANNEL_A);
-		PORTC	|= (1 << PORTC0);
-		PORTB	|= (1 << PORTB0);
-		Motors_MUX	= 0;
-	}
+	
+	usart_receive_bytes();
+	
 }
 
+// TIM0 OC0A interrupt routine. "TIM0_Count" is incremented, and depending it's value, the decoder's selector bits are correctly
+// established, and the correct OCR1A value is uploaded.
+ISR(TIMER0_COMPA_vect)
+{
+	cli();
+	
+	TIM0_Count++;
+	if (TIM0_Count == 8) TIM0_Count = 0;
+	switch (TIM0_Count)
+	{
+		case 0:
+			//PORTB	|= (1 << PORTB5) | (0 << PORTB4) | (0 << PORTB3) | (0 << PORTB2);
+			PORTB	&= ~(0x1C);
+			PORTB	|= (0 << PORTB4) | (0 << PORTB3) | (0 << PORTB2);
+			tim1_ocr_value(TIM1_CHANNEL_A, (uint16_t)ADCH_to_PWM[Motors.Usable[0]]);
+			tim1_tcnt_value(0);
+			break;
+		case 1:
+			//PORTB	|= (1 << PORTB5) | (0 << PORTB4) | (0 << PORTB3) | (1 << PORTB2);
+			PORTB	&= ~(0x1C);
+			PORTB	|= (0 << PORTB4) | (0 << PORTB3) | (1 << PORTB2);
+			tim1_ocr_value(TIM1_CHANNEL_A, (uint16_t)ADCH_to_PWM[Motors.Usable[1]]);
+			tim1_tcnt_value(0);
+			break;
+		case 2:
+			//PORTB	|= (1 << PORTB5) | (0 << PORTB4) | (1 << PORTB3) | (0 << PORTB2);
+			PORTB	&= ~(0x1C);
+			PORTB	|= (0 << PORTB4) | (1 << PORTB3) | (0 << PORTB2);
+			tim1_ocr_value(TIM1_CHANNEL_A, (uint16_t)ADCH_to_PWM[Motors.Usable[2]]);
+			tim1_tcnt_value(0);
+			break;
+		case 3:
+			//PORTB	|= (1 << PORTB5) | (0 << PORTB4) | (1 << PORTB3) | (1 << PORTB2);
+			PORTB	&= ~(0x1C);
+			PORTB	|= (0 << PORTB4) | (1 << PORTB3) | (1 << PORTB2);
+			tim1_ocr_value(TIM1_CHANNEL_A, (uint16_t)ADCH_to_PWM[Motors.Usable[3]]);
+			tim1_tcnt_value(0);
+			break;
+		case 4:
+			//PORTB	|= (1 << PORTB5) | (1 << PORTB4) | (0 << PORTB3) | (0 << PORTB2);
+			PORTB	&= ~(0x1C);
+			PORTB	|= (1 << PORTB4) | (0 << PORTB3) | (0 << PORTB2);
+			tim1_ocr_value(TIM1_CHANNEL_A, (uint16_t)ADCH_to_PWM[Motors.Usable[4]]);
+			tim1_tcnt_value(0);
+			break;
+		case 5:
+			//PORTB	|= (1 << PORTB5) | (1 << PORTB4) | (0 << PORTB3) | (1 << PORTB2);
+			PORTB	&= ~(0x1C);
+			PORTB	|= (1 << PORTB4) | (0 << PORTB3) | (1 << PORTB2);
+			tim1_ocr_value(TIM1_CHANNEL_A, (uint16_t)ADCH_to_PWM[Motors.Usable[5]]);
+			tim1_tcnt_value(0);
+			break;
+		case 6:
+			//PORTB	|= (1 << PORTB5) | (1 << PORTB4) | (1 << PORTB3) | (0 << PORTB2);
+			PORTB	&= ~(0x1C);
+			PORTB	|= (1 << PORTB4) | (1 << PORTB3) | (0 << PORTB2);
+			tim1_ocr_value(TIM1_CHANNEL_A, (uint16_t)ADCH_to_PWM[Motors.Usable[6]]);
+			tim1_tcnt_value(0);
+			break;
+		/*
+		case 7: 
+			PORTB	|= (1 << PORTB5) | (1 << PORTB4) | (1 << PORTB3) | (1 << PORTB2);
+			tim1_ocr_value(TIM1_CHANNEL_A, (uint16_t)ADCH_to_PWM[Motors.Usable[7]]);
+			tim1_tcnt_value(0);
+			break;
+		*/
+		default: break;
+	}
+	
+	sei();
+}
+
+// TIM1 OC1A interrupt routine. The decoder is disabled.
 ISR(TIMER1_COMPA_vect)
 {
 	
-	PORTB	&= ~(1 << PORTB0);
-	//tim1_oc_interrupt_disable(TIM1_CHANNEL_A);
+	cli();
+	
+	//if (TIM0_Count < 4) PORTB	&= ~(0x3C);
+	if (TIM0_Count < 7) PORTB	|= (1 << PORTB4) | (1 << PORTB3) | (1 << PORTB2);
+	
+	sei();
 	
 }
 
+// ADC interrupt routine. "ADC_Lec" is used for storing the lecture of the ADC, and "ADC_Count" tells to which index of the Motors.Manual
+// struct should the lecture be kept.
 ISR(ADC_vect)
 {
 	
-	ADC_Lec	= ADCH;
-	ADC_Lec	= (ADC_Lec * 250)/255;
+	cli();
 	
+	ADC_Lec = ADCH;
+	switch (ADC_Count)
+	{
+		case 0:
+		ADC_Count = 1;
+		Motors.Manual[0] = ADC_Lec;
+		adc_channel(ADC_CHANNEL_ADC6);
+		break;
+		case 1:
+		ADC_Count = 2;
+		Motors.Manual[1] = ADC_Lec;
+		adc_channel(ADC_CHANNEL_ADC5);
+		break;
+		case 2:
+		ADC_Count = 3;
+		Motors.Manual[2] = ADC_Lec;
+		adc_channel(ADC_CHANNEL_ADC4);
+		break;
+		case 3:
+		ADC_Count = 0;
+		Motors.Manual[3] = ADC_Lec;
+		adc_channel(ADC_CHANNEL_ADC7);
+		break;
+		default: break;
+	}
+	adc_start_conversion();
+	
+	sei();
 }
